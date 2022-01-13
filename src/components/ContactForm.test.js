6772ae74b1189146089adeb1b2f10177d23baa8d
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-01-13 14:54:24 -0800

    Finished Test 5 and 6

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -11,7 +11,9 @@ test('renders without errors', ()=>{
 
 test('renders the contact form header', ()=> {
   render(<ContactForm />);
+
   const header = screen.queryByText(/contact form/i);
+
   expect(header).toBeInTheDocument();
   expect(header).toBeTruthy();
   expect(header).toHaveTextContent(/contact form/i);
@@ -19,32 +21,57 @@ test('renders the contact form header', ()=> {
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
   render(<ContactForm />);
+
   const input = "Four";
   const firstName = screen.getByLabelText(/first name*/i);
   userEvent.type(firstName, input);
-  waitFor(async () => {
+
+  await waitFor(async () => {
     const error = screen.queryAllByTestId("error");
-    expect(error).toBeTruthy();
     expect(error).toHaveLength(1);
   });
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
   render(<ContactForm />);
+
   const submit = screen.getByRole("button");
   userEvent.click(submit);
-  waitFor(async () => {
+
+  await waitFor(async () => {
     const error = screen.queryAllByTestId("error");
     expect(error).toHaveLength(3);
   });
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+  render(<ContactForm />);
+
+  const input = "Valid";
+  const firstName = screen.getByLabelText(/first name*/i);
+  userEvent.type(firstName, input);
+  const lastName = screen.getByLabelText(/last name*/i);
+  userEvent.type(lastName, input);
+  const submit = screen.getByRole("button");
+  userEvent.click(submit);
+
+  await waitFor(async () => {
+    const error = screen.queryAllByTestId("error");
+    expect(error).toHaveLength(1);
+  });
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+  render(<ContactForm />);
+
+  const input = "invalidemail@nowhere";
+  const email = screen.getByLabelText(/email*/i);
+  userEvent.type(email, input);
+
+  await waitFor(async () => {
+    const error = screen.queryByTestId("error");
+    expect(error).toHaveTextContent(/email must be a valid email address/i);
+  });
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
